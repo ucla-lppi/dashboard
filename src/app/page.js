1edb8f8ffec44ab06b1429a7d810df6129d0ce5e
albertkun <albertk@gmx.com>
2024-09-11 12:41:01 -0700

    feat: add metadata to the California Latino Dashboard

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,21 @@
+import Head from 'next/head';
 import Dashboard from './components/Dashboard';
 
 export default function Home() {
-  return <Dashboard />;
+  return (
+    <>
+      <Head>
+        <title>California Latino Dashboard</title>
+        <meta name="description" content="The California Latino Dashboard by UCLA LPPI showcases climate change and health equity data for Latino neighborhoods in California." />
+        <meta name="keywords" content="California Latino Dashboard, UCLA LPPI, climate change, health equity, Latino communities" />
+        <meta name="author" content="UCLA Latino Policy and Politics Institute" />
+        <meta property="og:title" content="California Latino Dashboard" />
+        <meta property="og:description" content="The California Latino Dashboard by UCLA LPPI showcases climate change and health equity data for Latino neighborhoods in California." />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://ucla-lppi.github.io/dashboard" />
+        <meta property="og:image" content="images/metadata.png" />
+      </Head>
+      <Dashboard />
+    </>
+  );
 }
