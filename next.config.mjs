b9fd7768c8bf536cd64d56b11f348690cdb897b0
albertkun <albertk@gmx.com>
2024-12-11 16:37:52 -0800

    fix: deployment on aws and github pages

--- next.config.mjs
+++ next.config.mjs
@@ -11,15 +11,6 @@ const nextConfig = {
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
   assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX || '',
   trailingSlash: true,
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    return {
-      '/': { page: '/' },
-      // Add other pages here
-    }
-  },
 };
 
 export default withMDX(nextConfig);
