a4276bc1ac7527522ae4b9cbe76e76c9b7520bc3
albertkun <albertk@gmx.com>
2025-01-09 16:03:38 -0800

    chore: update deployment workflows for GitHub Pages and EC2 with improved environment variable handling

--- next.config.mjs
+++ next.config.mjs
@@ -12,8 +12,8 @@ const withMDX = mdx({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
-  assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX || '',
+  basePath: process.env.BASE_PATH || '',
+  assetPrefix: process.env.ASSET_PREFIX || '',
   trailingSlash: true,
   output: 'export',
 };
