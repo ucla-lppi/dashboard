7e2e826f9203f749d5ad480a19612150ef1edc61
albertkun <albertk@gmx.com>
2025-04-22 09:34:59 -0700

    fix: urls

--- src/app/components/CaliforniaMap.js
+++ src/app/components/CaliforniaMap.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef } from "react";
 import * as d3 from "d3";
 
-const geoJsonUrl = "/static/data/ca_counties.geojson"; // Serve from public static folder
+const geoJsonUrl = "./static/data/ca_counties.geojson"; // Serve from public static folder
 const options = {
 	mapHeight: '551'
 }
@@ -117,7 +117,7 @@ export default function CaliforniaMap() {
       <div className="absolute top-4 right-1 bg-white p-4 rounded-lg shadow-[3px_3px_0px_var(--quaternary-color)] border border-primary">
         <div className="flex items-center mb-2">
           <img
-            src="/images/extremeheaticon.svg"
+            src="./images/extremeheaticon.svg"
             alt="Extreme Heat Icon"
             width="27"
             height="23"
@@ -127,7 +127,7 @@ export default function CaliforniaMap() {
         </div>
         <div className="flex items-center">
           <img
-            src="/images/airpollutionicon.svg"
+            src="./images/airpollutionicon.svg"
             alt="Air Pollution Icon"
             width="27"
             height="23"
