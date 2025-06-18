ddde63b2b73b1b9183e0585be80127aeca4c8802
albertkun <albertk@gmx.com>
2025-06-18 14:58:28 -0700

    fix: adjust button visibility for mobile view in HomePage and CaliforniaCountyProfiles components

--- src/app/components/HomePage.js
+++ src/app/components/HomePage.js
@@ -54,9 +54,12 @@ export default function HomePage() {
           </div>
           <div className="flex flex-col items-start space-y-2">
             <div className="flex flex-col items-center">
-              <button onClick={() => document.getElementById('county-profiles').scrollIntoView({ behavior: 'smooth' })} className="text-m font-semibold text-black mb-1 text-center">
-                jump to <br />county factsheets
-              </button>
+              {/* Hide this button on mobile view */}
+              {!isMobile && (
+                <button onClick={() => document.getElementById('county-profiles').scrollIntoView({ behavior: 'smooth' })} className="text-m font-semibold text-black mb-1 text-center">
+                  jump to <br />county factsheets
+                </button>
+              )}
               <button onClick={() => document.getElementById('county-profiles').scrollIntoView({ behavior: 'smooth' })} className="bg-tertiary text-white p-3 rounded-full shadow-md" aria-label="Go down">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 transform rotate-90" fill="none" viewBox="0 0 24 24" stroke="#000" strokeWidth={2}><path strokeLinecap="round" strokeLinejoin="round" d="M5 12h14m-7-7l7 7-7 7" /></svg>
               </button>
